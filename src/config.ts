a291d3d21e3b0e0293d523146e9a237e1ee2e464
Dima Usov <dmitry.usov@yahoo.com>
2024-10-21 23:20:34 +0400

    fix: env variables

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,13 @@
 import { cpus } from 'node:os';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const DEFAULT_SERVER_PORT = 4000;
+const DEFAULT_DB_PORT = 3000;
 
 export const PORT = process.env.PORT || DEFAULT_SERVER_PORT;
+export const DB_PORT = process.env.DB_PORT || DEFAULT_DB_PORT;
 export const isMultiMode = process.argv.includes('--multi');
 export const cpuCount = cpus().length;
 
